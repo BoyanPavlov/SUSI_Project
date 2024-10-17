80b5d5982d57c25505fb4abf293b13ef42453b97
Boyan  Pavlov <boyan.v.pavlov@gmail.com>
2024-10-17 16:34:42 +0300

    feat: declared AuthService

---
+++ src/ServiceAutorization/AuthService.java
@@ -0,0 +1,11 @@
+package ServiceAutorization;
+
+public interface AuthService {
+
+    //TODO
+    boolean login(String username, String password);
+
+    //public void signOut();
+
+    //public UserData getSignedInUser();
+}
---
+++ src/ServiceAutorization/MyService.java
@@ -0,0 +1,10 @@
+package ServiceAutorization;
+
+public class MyService implements AuthService {
+
+    @Override
+    public boolean login(String username, String password) {
+        //TODO
+        return false;
+    }
+}
